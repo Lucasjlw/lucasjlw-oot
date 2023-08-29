7d5b890fc87647024d1acee4a15448ad27c54e62
Lucasjlw <lucasjlw@gmail.com>
2023-08-29 17:38:12 -0500

    Fix bug in HTTP Response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod server;
+use lucasjlw_oot::run;
 
 fn main() {
-    // run();
-    server::serve();
+    let args: Vec<String> = std::env::args().collect();
+
+    if &args[1] == "serve" {
+        println!("this is running");
+        server::serve("base.html");
+    } else {
+        run();
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,26 +1,25 @@
 use std::{
-    net::{TcpListener, TcpStream}, 
-    io::{Read, Write, BufReader}
+    net::{TcpListener}, 
+    io::{Read, Write}
 };
-use std::fs::File;
+use std::fs;
 
-pub fn serve() {
+pub fn serve(filePath: &str) {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
 
-        let mut htmlContentString = String::new();
-        let _ = File::open("base.html").unwrap().read_to_string(&mut htmlContentString);
-        let htmlContentLength = htmlContentString.len();
+        let htmlContent = fs::read_to_string(filePath).unwrap();
+        let htmlContentLength = htmlContent.len();
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\n
-            Content-Type: text/html; charset=UTF-8\r\n
-            Content-Length: {htmlContentLength}\r\n\r\n
-            {htmlContentString}",
-        );
+        let mut response = String::new();
+        response.push_str("HTTP/1.1 200 OK\r\n");
+        response.push_str("Content-Type: text/html; charset=UTF-8\r\n");
+        response.push_str(format!("Content-Length: {htmlContentLength}\r\n\r\n").as_str());
+        response.push_str(htmlContent.as_str());
+
 
         stream.write_all(response.as_bytes()).unwrap();
     }
-}
+}   
